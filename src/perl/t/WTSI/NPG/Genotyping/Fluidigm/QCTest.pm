7fe55a2e64533deda097dffbb3c91bc8643bec9a
iainrb <ib5@sanger.ac.uk>
2017-02-21 15:35:07 +0000

    Additional tests for script

--- src/perl/t/WTSI/NPG/Genotyping/Fluidigm/QCTest.pm
+++ src/perl/t/WTSI/NPG/Genotyping/Fluidigm/QCTest.pm
@@ -8,11 +8,12 @@ use warnings;
 use base qw(WTSI::NPG::Test);
 use File::Copy qw/copy/;
 use File::Temp qw/tempdir/;
-use Test::More tests => 7;
+use Test::More tests => 10;
 use Test::Exception;
 use Text::CSV;
 
-Log::Log4perl::init('./etc/log4perl_tests.conf');
+our $logconf = './etc/log4perl_tests.conf';
+Log::Log4perl::init($logconf);
 our $log = Log::Log4perl->get_logger();
 
 BEGIN { use_ok('WTSI::NPG::Genotyping::Fluidigm::QC'); }
@@ -23,7 +24,9 @@ use WTSI::NPG::iRODS;
 use WTSI::NPG::iRODS::Metadata; # has attribute name constants
 use WTSI::NPG::Utilities qw(md5sum);
 
-my $data_path = './t/fluidigm_qc/1381735059';
+my $script = 'qc_fluidigm.pl';
+my $plate = '1381735059';
+my $data_path = "./t/fluidigm_qc/$plate";
 my $irods_tmp_coll;
 my $pid = $$;
 my $tmp;
@@ -49,10 +52,9 @@ sub require : Test(1) {
 sub update : Test(4) {
     my $irods = WTSI::NPG::iRODS->new;
     my @irods_paths;
-    my $barcode = '1381735059';
     foreach my $prefix (qw/S01 S02/) {
-        my $data_file = $prefix.'_'.$barcode.'.csv';
-        my $irods_path = "$irods_tmp_coll/$barcode/$data_file";
+        my $data_file = $prefix.'_'.$plate.'.csv';
+        my $irods_path = "$irods_tmp_coll/$plate/$data_file";
         push @irods_paths, $irods_path;
     }
     my @data_objects;
@@ -97,15 +99,101 @@ sub update : Test(4) {
               'Update string contents match expected values');
 }
 
-sub script : Test(1) {
-
-    my $script = 'qc_fluidigm.pl';
-
+sub script_metaquery : Test(2) {
+    my $irods = WTSI::NPG::iRODS->new;
+    my %wells = (
+        'S01_1381735059.csv' => 'S01',
+        'S02_1381735059.csv' => 'S02',
+    );
+    foreach my $name (keys %wells) {
+        my $irods_path = $irods_tmp_coll.'/'.$plate.'/'.$name;
+        $irods->add_object_avu($irods_path, 'type', 'csv');
+        $irods->add_object_avu($irods_path, 'fluidigm_plate', $plate);
+        $irods->add_object_avu($irods_path, 'fluidigm_well', $wells{$name});
+    }
     my $cmd = "$script --query-path $irods_tmp_coll ".
-        "--old-csv $tmp/$csv_name --in-place";
+        "--old-csv $tmp/$csv_name --in-place --logconf $logconf";
+    $log->info("Running command '$cmd'");
+    ok(system($cmd)==0, "Script with --in-place and metaquery exits OK");
+    my $csv = Text::CSV->new ( { binary => 1 } );
+    open my $fh, "<", "$tmp/$csv_name" ||
+        $log->logcroak("Cannot open input '$tmp/$csv_name'");
+    my $contents = $csv->getline_all($fh);
+    close $fh || $log->logcroak("Cannot close input '$tmp/$csv_name'");
+    my $expected_contents = [
+        [
+            'ABC0123456789',
+            '1.0000',
+            96,
+            96,
+            70,
+            70,
+            96,
+            26,
+            26,
+            '11413e77cde2a8dcca89705fe5b25a2d',
+        ], [
+            'XYZ0987654321',
+            '0.9231',
+            96,
+            94,
+            70,
+            70,
+            96,
+            26,
+            24,
+            '73ca301a0a9e1b9cf87d4daf59eb2815',
+        ],
+    ];
 
-    ok(system($cmd)==0, "Script with in-place in effect exits OK");
+    is_deeply($contents, $expected_contents,
+              "Script in-place CSV output matches expected values");
+}
 
+sub script_stdin : Test(2) {
+    my @irods_paths;
+    foreach my $prefix (qw/S01 S02/) {
+        my $data_file = $prefix.'_'.$plate.'.csv';
+        my $irods_path = "$irods_tmp_coll/$plate/$data_file";
+        push @irods_paths, $irods_path;
+    }
+    my $fh;
+    my $input_path = $tmp."/test_inputs.txt";
+    open $fh, ">", $input_path ||
+        $log->logcroak("Cannot open '$input_path'");
+    foreach my $path (@irods_paths) {
+        print $fh $path."\n";
+    }
+    close $fh || $log->logcroak("Cannot close '$input_path'");
+    my $new_csv = "$tmp/fluidigm_qc_output.csv";
+    my $cmd = "$script --new-csv $new_csv --old-csv $tmp/$csv_name ".
+        "--logconf $logconf - < $input_path";
+    $log->info("Running command '$cmd'");
+    ok(system($cmd)==0, "Script with STDIN and new CSV file exits OK");
+
+    # check the CSV output
+    my $csv = Text::CSV->new ( { binary => 1 } );
+    open $fh, "<", "$new_csv" ||
+        $log->logcroak("Cannot open input '$new_csv'");
+    my $contents = $csv->getline_all($fh);
+    close $fh || $log->logcroak("Cannot close input '$new_csv'");
+    my $expected_contents = [
+        [
+            'XYZ0987654321',
+            '0.9231',
+            96,
+            94,
+            70,
+            70,
+            96,
+            26,
+            24,
+            '73ca301a0a9e1b9cf87d4daf59eb2815',
+        ],
+    ];
+
+    is_deeply($contents, $expected_contents,
+              "New CSV output from script matches expected values");
 }
 
 
