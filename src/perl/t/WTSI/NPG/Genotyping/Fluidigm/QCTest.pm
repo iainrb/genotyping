bccc08816ddceda3a9d69b473b4aea5fbaf6b970
iainrb <ib5@sanger.ac.uk>
2017-02-24 11:29:22 +0000

    Added more method tests

--- src/perl/t/WTSI/NPG/Genotyping/Fluidigm/QCTest.pm
+++ src/perl/t/WTSI/NPG/Genotyping/Fluidigm/QCTest.pm
@@ -7,8 +7,10 @@ use warnings;
 
 use base qw(WTSI::NPG::Test);
 use File::Copy qw/copy/;
+use File::Slurp qw/read_file/;
 use File::Temp qw/tempdir/;
-use Test::More tests => 13;
+use Set::Scalar;
+use Test::More tests => 17;
 use Test::Exception;
 use Text::CSV;
 
@@ -62,23 +64,10 @@ sub require : Test(1) {
 }
 
 sub csv_output : Test(5) {
-    my $irods = WTSI::NPG::iRODS->new;
-    my @data_objects;
-    # 1 of the 2 AssayDataObjects is already present in fluidigm_qc.csv
-    # updated contents will contain QC results for the other AssayDataObject
-    foreach my $irods_path (@irods_paths) {
-        my $obj = WTSI::NPG::Genotyping::Fluidigm::AssayDataObject->new(
-            $irods, $irods_path,
-        );
-        push @data_objects, $obj;
-    }
-    my $csv_path = "$tmp/$csv_name";
-    my $qc = WTSI::NPG::Genotyping::Fluidigm::QC->new(
-        csv_path     => $csv_path,
-        data_objects => \@data_objects,
-    );
+    my $qc = _create_qc_object();
+    my $data_objects = $qc->data_objects();
     my $fields;
-    lives_ok(sub {$fields = $qc->csv_fields($data_objects[1]); },
+    lives_ok(sub {$fields = $qc->csv_fields($data_objects->[1]); },
              'CSV fields found OK');
     my $expected_fields = [
         'XYZ0987654321',
@@ -97,15 +86,33 @@ sub csv_output : Test(5) {
     is_deeply($fields, $expected_fields,
               'Field contents match expected values');
     my $string;
-    lives_ok(sub {$string = $qc->csv_string($data_objects[1]); },
+    lives_ok(sub {$string = $qc->csv_string($data_objects->[1]); },
              'CSV string found OK');
     my $expected_string = 'XYZ0987654321,0.9231,96,94,70,70,96,26,24,'.
         '1381735059,S02,73ca301a0a9e1b9cf87d4daf59eb2815';
     ok($string eq $expected_string,
        'CSV string contents match expected values');
-    $data_objects[0]->remove_avu($FLUIDIGM_PLATE_WELL, 'S01');
-    dies_ok(sub { $qc->csv_fields($data_objects[0]); },
+
+    $data_objects->[0]->remove_avu($FLUIDIGM_PLATE_WELL, 'S01');
+    dies_ok(sub { $qc->csv_fields($data_objects->[0]); },
             'Dies without required metadata');
+}
+
+sub rewrite_existing : Test(2) {
+    my $qc = _create_qc_object();
+    my $output_path = "$tmp/rewritten.csv";
+    open my $fh, ">", $output_path ||
+        $log->logcroak("Cannot open CSV output ", $output_path);
+    my $got_md5 = $qc->rewrite_existing_csv($fh);
+    close $fh || $log->logcroak("Cannot close CSV output ", $output_path);
+    my $expected_md5 = Set::Scalar->new();
+    my $md5 = '11413e77cde2a8dcca89705fe5b25a2d';
+    $expected_md5->insert($md5);
+    is_deeply($got_md5, $expected_md5, 'MD5 sets match');
+    my $contents = read_file($output_path);
+    my $expected = 'ABC0123456789,1.0000,96,96,70,70,96,26,26,1381735059'.
+        ",S01,$md5\n";
+    cmp_ok($contents, 'eq', $expected, 'Rewritten CSV file contents OK');
 }
 
 sub script_metaquery : Test(2) {
@@ -128,7 +135,6 @@ sub script_update : Test(2) {
                          'Script updated md5 checksum in CSV');
 }
 
-
 sub script_stdin : Test(2) {
     my $fh;
     my $input_path = $tmp."/test_inputs.txt";
@@ -146,6 +152,34 @@ sub script_stdin : Test(2) {
      _validate_csv_output($new_csv, 'Script CSV output OK, input from STDIN');
 }
 
+sub write_all : Test(2) {
+    my $qc = _create_qc_object();
+    my $output_path = "$tmp/qc_output.csv";
+    open my $fh, ">", $output_path ||
+        $log->logcroak("Cannot open CSV output ", $output_path);
+    ok($qc->write_csv($fh), 'write_csv method returns OK');
+    close $fh || $log->logcroak("Cannot close CSV output ", $output_path);
+    _validate_csv_output($output_path, 'CSV output from QC object OK');
+}
+
+sub _create_qc_object {
+    my $irods = WTSI::NPG::iRODS->new;
+    my @data_objects;
+    # 1 of the 2 AssayDataObjects is already present in fluidigm_qc.csv
+    # updated contents will contain QC results for the other AssayDataObject
+    foreach my $irods_path (@irods_paths) {
+        my $obj = WTSI::NPG::Genotyping::Fluidigm::AssayDataObject->new(
+            $irods, $irods_path,
+        );
+        push @data_objects, $obj;
+    }
+    my $csv_path = "$tmp/$csv_name";
+    my $qc = WTSI::NPG::Genotyping::Fluidigm::QC->new(
+        csv_path     => $csv_path,
+        data_objects => \@data_objects,
+    );
+    return $qc;
+}
 
 sub _validate_csv_output {
     # check that CSV output matches the expected values
